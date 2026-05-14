5889055e3e9353fa25ae1f8de2818e57f43c4d8d
Hyeonjoong Jang <55046566+Hyeonjoong-Jang@users.noreply.github.com>
2026-05-14 23:22:38 +0900

    Update Ello tumbler weight

--- data/tumblers.demo.js
+++ data/tumblers.demo.js
@@ -334,7 +334,7 @@ window.SPECHECKED_TUMBLERS = [
     "specs": {
       "mouthDiameterCm": 8.4,
       "capacityMl": 530,
-      "weightG": 420,
+      "weightG": 419,
       "leakproof": false,
       "ceramicCoated": true,
       "lidFullyDisassemblable": true,
